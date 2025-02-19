3ce4ba39eca83a57c3d0f777868e6ab6e4c307b2
Ayman Salman <salman.ayman.87@gmail.com>
2025-02-19 10:59:09 +0200

    added contact us details page and html

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from .forms import ContactForm
+
+def contact_us(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            send_mail(
+                subject=f"Contact Us Message from {name}",
+                message=message,
+                from_email=email,
+                recipient_list=[os.environ.get('CONTACT_EMAIL')],
+            )
+            return redirect('contact_us_thanks')
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact_us/contact_us.html', {'form': form})
+
+def thanks(request):
+    return render(request, 'contact_us/thanks.html')
